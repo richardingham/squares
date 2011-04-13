269ec6c8c1bd2fc4da63f9e283be8703185b483a
richardingham <mail@richardingham.net>
2011-04-13 20:38:41 +0000

    Fixed typo in cell.js

--- cell.js
+++ cell.js
@@ -1,9 +1,72 @@
-var squares = {};
 
-suares.Cell = function () {
+squares.Cell = function (parent, formula, value) {
+    var changed = value !== null;
+    var dependents = [];
     
+    this.formula = function (set) {
+        if (set) {
+            var temp = formula;
+            formula = set;
+            changed = true;
+            return temp;
+        } else {
+            return formula;
+        }
+    }; 
+    
+    this.value = function () {
+        if (changed) {
+            this.update();
+        }
+
+        return value;
+    };
+    
+    this.parent = function () {
+        return parent;
+    };
+    
+    this.update = function () {
+        value = squares.Cell.compute(this);
+        return this;
+    };
+    
+    this.addDependent = function (cell) {
+        if (dependents.indexOf(cell) === -1) {
+            if (cell.parent() !== parent) {
+                return;
+            }
+            
+            dependents.push(cell);
+        }
+        
+        return this;
+    };
+    
+    this.removeDependent = function (cell) {
+        var index = dependents.indexOf(cell);
+        
+        if (index !== -1)
+            dependents.remove(index);
+        
+        return this;
+    };
 };
 
-
+squares.Cell.compute = function (cell) {
+    var parent = cell.parent();
+    var formula = cell.formula();
+    
+    // keep track of dependencies
+    if (formula[0] === '=') {
+        // TODO: lexer...
+        
+        // parent.hasChild("A2");
+        // parent.getChild("A2");
+        // parent.hasChild("weight");
+    } else {
+        return formula;
+    }
+};
 
 
